40516361b6c8a628b4db291d39e14d918e8c3d57
Harsh Mishra <harshmishra27122003@gmail.com>
2025-09-27 14:54:44 +0530

    Initial Spring Boot Kotlin project setup

---
+++ src/main/kotlin/com/example/productsfullstacktest/ProductsFullStackTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.productsfullstacktest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductsFullStackTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductsFullStackTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/productsfullstacktest/ProductsFullStackTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.productsfullstacktest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductsFullStackTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
